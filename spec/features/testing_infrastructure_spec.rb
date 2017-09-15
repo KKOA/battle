dc8245fbd21616a00493abd9b4652ee0022318c9
keith(mac) <ka33145@googlemail.com>
2017-09-15 12:38:41 +0100

    Added test for entering names and outputting them

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,18 @@
-feature 'homepage' do #describe
-  scenario 'return content' do #context
+feature 'homepage' do # describe
+  scenario 'return homepage' do # context
     visit('/')
     expect(page).to have_content 'Enter your names to do
 !! Battle !!'
-    #page - current page
+    # page - current page
+  end
+end
+
+feature 'Enter names' do
+  scenario 'return player names' do
+    visit('/')
+    fill_in(:player1_name, with: 'Jenkings')
+    fill_in(:player2_name, with: 'Rory')
+    click_button('Battle')
+    expect(page).to have_content('Jenkings vs Rory')
   end
 end
