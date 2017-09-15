e7da0d4b93cb3c03492b4862cf382a9663b4b3f1
keith(mac) <ka33145@googlemail.com>
2017-09-15 12:43:42 +0100

    Added route /names and added play.erb

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class Battle < Sinatra::Base
   get '/' do
     erb(:index)
   end
+
+  post '/names' do
+    @player1 = params[:player1_name]
+    @player2 = params[:player2_name]
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 end
