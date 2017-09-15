1af2a2860ae9f11f4ef3e2bc619e1bf0e5c4e201
keith(mac) <ka33145@googlemail.com>
2017-09-15 15:03:26 +0100

    Add route /attack to app.rb

--- app.rb
+++ app.rb
@@ -19,6 +19,13 @@ get '/play' do
   @player2 = session[:player2]
   erb(:play)
 end
+get '/attack' do
+  @player1 = session[:player1]
+  @player2 = session[:player2]
+  erb(:attack)
+end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
