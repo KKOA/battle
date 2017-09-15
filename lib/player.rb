9ec9d007d468d275552481dbfa75355a9db50677
keith(mac) <ka33145@googlemail.com>
2017-09-15 20:02:03 +0100

    Add hp instance variable, default hp constant and reader for hp

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,9 @@
 class Player
-  attr_reader :name
-  def initialize(name)
+  attr_reader :name, :hp
+
+  DEFAULT_HP = 60
+  def initialize(name,hp = DEFAULT_HP)
     @name = name
+    @hp = hp
   end
 end
