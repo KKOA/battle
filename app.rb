26639fda67437897f283b2b2b9c897e4459a53f3
keith(mac) <ka33145@googlemail.com>
2017-09-15 07:53:31 +0100

    Added root route to app.rb and added index.erb

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   # ... app code here ...
-
+  get '/' do
+    erb(:index)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 end
