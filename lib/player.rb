39e7ed274fb3619783922b348e49bc9060ae9a16
keith(mac) <ka33145@googlemail.com>
2017-09-15 21:14:43 +0100

    Added method attack and take_damage to player class

--- lib/player.rb
+++ lib/player.rb
@@ -2,8 +2,16 @@ class Player
   attr_reader :name, :hp
 
   DEFAULT_HP = 60
-  def initialize(name,hp = DEFAULT_HP)
+  def initialize(name, hp = DEFAULT_HP)
     @name = name
     @hp = hp
+  end
+
+  def attack(player)
+    player.take_damage
+  end
+
+  def take_damage
+    @hp -= 10
   end
 end
