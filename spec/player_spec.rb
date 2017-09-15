2b64dde0a032a16fece1ca03024cd5d9a0faf34f
keith(mac) <ka33145@googlemail.com>
2017-09-15 19:45:37 +0100

    Added test confirm plays initial hp

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,4 +6,11 @@ describe Player do
       expect(player.name).to eq 'Tom'
     end
   end
+  describe '#hp' do
+    it 'return player health' do
+      player = Player.new('Tom')
+      expect(player.hp).to eq Player::DEFAULT_HP
+    end
+  end
+
 end
