6a79cafa72b96adf648bca54028ec57a867c2767
keith(mac) <ka33145@googlemail.com>
2017-09-18 22:47:21 +0100

    Refactored code to store player instances inside game object

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # my_app.rb
 require 'sinatra/base'
 require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   set :sessions, true
@@ -13,22 +14,23 @@ class Battle < Sinatra::Base
       session['err'] = 'Player name cannot be empty'
       redirect('/')
     end
-    $player1 = Player.new(params[:player1_name])
-    $player2 = Player.new(params[:player2_name])
+    player1 = Player.new(params[:player1_name])
+    player2 = Player.new(params[:player2_name])
+    $game = Game.new(player1, player2)
     redirect('/play')
   end
 
   get '/play' do
-    @player1 = $player1
-    @player2 = $player2
+    @player1 = $game.player1
+    @player2 = $game.player2
     erb(:play)
   end
 
   get '/attack' do
 
-    @player1 = $player1
-    @player2 = $player2
-    Game.new.attack(@player2)
+    @player1 = $game.player1
+    @player2 = $game.player2
+    $game.attack(@player2)
     erb(:attack)
   end
   # start the server if ruby file executed directly
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,25 @@
 require 'player'
 require 'game'
 describe Game do
+  subject(:player1) { Player.new('Tom') }
   subject(:player2) { Player.new('Mary') }
-  subject(:game) { Game.new }
+  subject(:game) { Game.new(player1, player2) }
 
   describe '#attack' do
     it 'deal damage to opponent' do
       expect { game.attack(player2) }.to change { player2.hp }.by(-10)
+    end
+  end
+
+  describe '#player1' do
+    it 'return player1 object' do
+      expect(game.player1).to eq player1
+    end
+  end
+
+  describe '#player2' do
+    it 'return player2 object' do
+      expect(game.player2).to eq player2
     end
   end
 end
